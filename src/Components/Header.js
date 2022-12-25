88a8b70f832a55d0ae7710c9099b42991b9de4bd
Blake Creasser <blakecreasser@aggienetwork.com>
2022-12-25 17:22:27 -0600

    change style of header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -4,9 +4,9 @@ function Header(){
     return (
         <div className="header">
             <h1>Creasser Creations</h1>
-            <a href="https://github.com/Creasser" className="header-link">Github</a>
-            <a href="https://www.linkedin.com/in/blake-creasser-701777242/" className="header-link">Linkedin</a>
-            <a href="https://dev.to/creasser" className="header-link">Blog</a>
+            <a href="https://github.com/Creasser" className="header-links">Github</a>
+            <a href="https://www.linkedin.com/in/blake-creasser-701777242/" className="header-links">Linkedin</a>
+            <a href="https://dev.to/creasser" className="header-links">Blog</a>
         </div>
     )
 }
