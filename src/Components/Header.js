3c53cad386c8f508c3d49af202ab3d58d0cc174a
Blake Creasser <blakecreasser@aggienetwork.com>
2022-12-25 16:41:39 -0600

    add header component and start styling

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -2,8 +2,11 @@ import React from "react";
 
 function Header(){
     return (
-        <div>
+        <div className="header">
             <h1>Creasser Creations</h1>
+            <a href="https://github.com/Creasser" className="header-link">Github</a>
+            <a href="https://www.linkedin.com/in/blake-creasser-701777242/" className="header-link">Linkedin</a>
+            <a href="https://dev.to/creasser" className="header-link">Blog</a>
         </div>
     )
 }
